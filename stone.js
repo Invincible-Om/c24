4dc1d0c6e9241477bb6cc49218c21c9d85676703
omanshom <76990881+omanshom@users.noreply.github.com>
2021-04-17 11:55:59 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -5,7 +5,7 @@ class Stone {
         'friction': 0.9,
         'restitution':0.8
       };
-      this.body = Bodies.rectangle(x, y, 100, 100, options);
+      this.body = Bodies.rectangle(x, y, 50, 60, options);
       this.width = 50;
       this.height = 60;
       World.add(world, this.body);
